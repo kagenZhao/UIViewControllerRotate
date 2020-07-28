b179a2c6caed4c81d2f90fc212c5010766218b20
赵国庆 <kagen@kagenz.com>
2020-07-28 14:31:08 +0800

    添加statusBar 测试

---
+++ Example/UIViewControllerRotation/ViewController11.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController11.swift
+//  UIViewControllerRotation_Example
+//
+//  Created by Kagen Zhao on 2020/7/28.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController11: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+
+}
---
+++ Example/UIViewControllerRotation/ViewController12.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController12.swift
+//  UIViewControllerRotation_Example
+//
+//  Created by Kagen Zhao on 2020/7/28.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController12: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
