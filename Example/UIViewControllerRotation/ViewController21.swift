751d71cb11381edf4dbcaf20df4ac28b6d627a3f
赵国庆 <kagen@kagenz.com>
2020-07-28 14:31:08 +0800

    添加statusBar 测试

---
+++ Example/UIViewControllerRotation/ViewController21.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController21.swift
+//  UIViewControllerRotation_Example
+//
+//  Created by Kagen Zhao on 2020/7/28.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController21: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+}
