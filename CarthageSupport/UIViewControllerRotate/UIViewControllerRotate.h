c353c80c259681f0b9e9d5f885c00e9f89cdeac5
赵国庆 <kagen@kagenz.com>
2021-06-10 15:17:57 +0800

    update

---
+++ CarthageSupport/UIViewControllerRotate/UIViewControllerRotate.h
@@ -0,0 +1,18 @@
+//
+//  UIViewControllerRotate.h
+//  UIViewControllerRotate
+//
+//  Created by Kagen Zhao on 2021/6/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIViewControllerRotate.
+FOUNDATION_EXPORT double UIViewControllerRotateVersionNumber;
+
+//! Project version string for UIViewControllerRotate.
+FOUNDATION_EXPORT const unsigned char UIViewControllerRotateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewControllerRotate/PublicHeader.h>
+
+
